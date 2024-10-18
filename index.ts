b8f16e6f952b4e640a2e082a57262bfa3c9d3261
Manu <emanuelly.viana@etec.sp.gov.br>
2024-10-17 21:49:36 -0300

    Meu primeiro commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+console.log("Olá");
+
+/* "git init" -> inicializa o git
+"git remote add origin" -> cria um local entre o repositório local
+e o repositório no provedor de repositórios.
+"git add" -> adiciona os arquivos no repositório.
+"git status" -> lista os arquivos (adicionados, modificados);
+
+A PASTA PRECISA ESTAR NA ÁREA DE TRABALHO!!!
+*/
