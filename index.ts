d1fca27c0ec51276f446bddf63046fe75aaaa183
clone532 <emanuelly.viana@etec.sp.gov.br>
2024-10-17 22:07:24 -0300

    Terceiro commit

--- index.ts
+++ index.ts
@@ -3,8 +3,10 @@ console.log("Olá");
 /* "git init" -> inicializa o git
 "git remote add origin" -> cria um local entre o repositório local
 e o repositório no provedor de repositórios.
-"git add" -> adiciona os arquivos no repositório.
+"git add ." -> adiciona os arquivos no repositório.
 "git status" -> lista os arquivos (adicionados, modificados);
+"git commit -m "commit name" -> salva o arquivo basicamente
+"git push origin master" -> manda o commit
 
 A PASTA PRECISA ESTAR NA ÁREA DE TRABALHO!!!
 */
